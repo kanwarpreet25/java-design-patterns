383803e0c4605a04c519766a1bddeb6b860403c4
kanwar <kanwarpreet25@gmail.com>
2018-11-26 00:23:48 +0530

    component pattern added

---
+++ component/src/test/java/com/iluwatar/composite/AppTest.java
@@ -0,0 +1,20 @@
+package com.iluwatar.composite;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+
+    @Test
+    public void testApp()
+    {
+    	String[] args = {};
+        App.main(args);
+    }
+}
